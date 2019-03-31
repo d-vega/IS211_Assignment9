10ac14393a2832014cbfe4c2eaafe2613265947e
Diandra <diandrav@DESKTOP-KDJAQVJ.localdomain>
2019-03-31 19:11:09 -0400

    Completed football_stats.py

--- football_stats.py
+++ football_stats.py
@@ -5,33 +5,33 @@
 
 from bs4 import BeautifulSoup
 import urllib2
-import json
 
 
 TOUCHDOWNS_PG = urllib2.urlopen("https://www.cbssports.com/nfl/stats/player"
                                 "sort/nfl/year-2018-season-regular-"
                                 "category-touchdowns")
-soup = BeautifulSoup(TOUCHDOWNS_PG.read(), features='lxml')
-players = soup.find_all("tr", class_={"row1", "row2"})
-players_td = soup.find_all("td", class_={"sort"})
-plyr_list = []
-finallist = []
-counter = 0
+SOUP = BeautifulSoup(TOUCHDOWNS_PG.read(), features='lxml')
+PLAYERS = SOUP.find_all("tr", class_={"row1", "row2"})
+PLYR_LIST = []
+COUNTER = 0
+POSITION = 0
 
 
-for a_tag in players:
+for a_tag in PLAYERS:
     match = a_tag.find_all('a')
     touchdowns = a_tag.find_all("td", class_={"sort"})
     name_team = [i.text for i in match]
     plyr_td = [t.text for t in touchdowns]
-    plyr_list.append(name_team)
+    PLYR_LIST.append(name_team)
     for touchdown in plyr_td:
-        plyr_list[counter].append(touchdown)
-    counter += 1
+        PLYR_LIST[COUNTER].append(touchdown)
+    COUNTER += 1
 
 
-for plyr_data in plyr_list:
-    finallist.append(json.dumps(plyr_data))
-
-
-print json.dumps(plyr_list[0][0])
+for player in PLYR_LIST[:20]:
+    POSITION += 1
+    info = "{}. {} has {} touchdown(s) and is from team {}.".format(POSITION,
+                                                                    player[0],
+                                                                    player[2],
+                                                                    player[1])
+    print info
